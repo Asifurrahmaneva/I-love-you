e65e1d72f4fa702392262e74552205c423d8496d
Asifurrahmaneva <114900886+Asifurrahmaneva@users.noreply.github.com>
2022-10-03 12:09:17 +0600

    Create main.js

---
+++ main.js
@@ -0,0 +1,148 @@
+/*const number = 10;
+
+for (let num= 1; num <= number ; num++){
+document.write("" + num + "<br/>");
+}*/
+
+/*
+const name = [" d", " c", " b"," a"];
+document.write(name.sort());*/
+
+/*
+const number =[9,8,7,6,5,4,3,2,1];
+console.log(number.sort(function(a,b){
+  return a-b; /*বড় থেকে ছোট b - a /
+}));*/
+/*
+const vowels = ["a", "e", "i", "o", "u","A", "E", "I", "O", "U"]
+
+function countVowels (sentence){
+let count = 0;
+const letters = Array.from(sentence);
+
+letters.forEach(function(value) {
+if (vowels . includes (value)){
+count++;
+}
+});
+return count;
+}
+console.log (countVowels ("I love Bangladesh"));*/
+
+
+let time = "1";
+setInterval(function(){
+document.write("Hello miss eva I love you 😘" + time++ +"<br/>");
+},50);
+
+
+
+/*
+
+
+let num = 5;
+let text ="";
+while(1 < num){
+  text += "the number is" + num;
+  num++;
+};
+document.write(text);
+
+server crash😀😀😀😀
+*/
+/*
+
+let time = "01010101010101010101010101010101010101";
+setInterval(function(){
+document.write(" " + time +"<br/>");
+},1000);
+*/
+
+
+/*
+let i = 0;
+let text ="";
+
+while (i < 10){
+  text += " the number is " + i + "<br/>" ;
+  i++;
+};
+document.write(text);
+
+*/
+
+/*
+let i = 0;
+let j = "";
+while(i < 10){
+  j += i;
+  i++;
+}
+
+document.write(j);
+
+*/
+/*
+let num = 5;
+let text ="";
+while(1 < num){
+  text += "the number is" + num;
+  num++;
+};
+document.write(text);
+*/
+/*
+
+const vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"]
+
+function countVowels(sentence) {
+  let count = 0;
+  const letters = Array.from(sentence);
+
+  letters.forEach(function(value) {
+    if (vowels.includes(value)) {
+      count++;
+    }
+  });
+  return count;
+}
+document.write(countVowels("shariar adib bond"));*/
+/*
+const vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"]
+const input = document.querySelector("input")
+const btn = document.querySelector("button")
+
+btn.addEventListener("click", () => {
+
+  let output = countVowels(input.value)
+
+  document.write(output)
+})
+
+function countVowels(sentence) {
+  let count = 0;
+  const letters = Array.from(sentence);
+
+  letters.forEach(function(value) {
+    if (vowels.includes(value)) {
+      count++;
+    }
+  });
+  return count;
+}
+*/
+
+/*
+let time = "1";
+setInterval(function() {
+  document.write("number " + time++ + "<br/>");
+}, 1000); */
+/*
+let num = 5;
+let text = "";
+while (1 < num) {
+  text += "the number is" + num;
+  num++;
+};
+document.write(text);
+*/
